1244bd42fbf604e75b235b8549f363867b569dbe
Fawaz Farid <fawwazally@gmail.com>
2019-08-21 03:09:46 +0300

    Initial Commit

---
+++ spec/mpesa_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Mpesa do
+  it "has a version number" do
+    expect(Mpesa::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
