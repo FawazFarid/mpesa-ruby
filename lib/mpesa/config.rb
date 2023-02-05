92564ef5b530f8d49c30a195a8dc75061a0b409d
Fawaz Farid <fawwazally@gmail.com>
2023-02-05 17:27:21 +0300

    Add comment on sandbox_passkey constant

--- lib/mpesa/config.rb
+++ lib/mpesa/config.rb
@@ -13,6 +13,8 @@ module Mpesa
 
     LIVE_API_BASE = 'https://api.safaricom.co.ke/'.freeze
 
+    # This passkey is for testing purposes only on sandbox. For production, you should use your own passkey
+    # that you get from Safaricom.
     SANDBOX_PASSKEY = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'.freeze
 
     # @return [String] your app's client key. This value *must* be set.
